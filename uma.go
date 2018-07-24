80dd99daaf64795d8ef849060f1f2882307b2a40
Hugo Cortes <hcortes@mydevices.com>
2018-07-24 01:56:48 -0700

    feat(uma): add user view by token

---
+++ uma.go
@@ -0,0 +1,32 @@
+package keycloak
+
+import (
+	"context"
+	"fmt"
+)
+
+// UMAService handles communication with Keycloak UMA
+type UMAService service
+
+// GetUMAUser allows user to view their token mappings.
+// The provided interface is returned to be decoded on success.
+func (c *UMAService) GetUMAUser(
+	ctx context.Context,
+	token string,
+	v interface{},
+) (interface{}, *Response, error) {
+	path := fmt.Sprintf("%s/%s/protocol/openid-connect/userinfo", defaultBase, c.client.realm)
+	h := headers{authorization: token}
+
+	req, err := c.client.newRequest("GET", path, nil, h)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := c.client.do(ctx, req, v)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return v, resp, nil
+}
